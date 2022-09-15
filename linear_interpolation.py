df4fcb100a5b4d9687d2c58715b581501d4a9320
arturxe2 <arturxe@gmail.com>
2022-09-15 09:54:02 +0200

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -22,7 +22,6 @@ def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features',
         feat_half1 = np.load(os.path.join(path, game, '1_' + input_name))
         feat_half2 = np.load(os.path.join(path, game, '2_' + input_name))
         
-        print(feat_half1)
         feat_half1_2fps = []
         for i in range(len(feat_half1) - 1):
             feat_aux = (feat_half1[i, :] + feat_half1[i+1, :]) / 2
@@ -30,9 +29,17 @@ def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features',
             feat_half1_2fps.append(feat_aux)
             
         feat_half1_2fps.append(feat_half1[-1, :])
-        print(np.array(feat_half1_2fps).shape)
-        print(np.array(feat_half1_2fps))
-        print(asdf)
+        feat_half1_2fps = np.array(feat_half1_2fps)
+        
+        feat_half2_2fps = []
+        for i in range(len(feat_half2) - 1):
+            feat_aux = (feat_half2[i, :] + feat_half2[i+1, :]) / 2
+            feat_half2_2fps.append(feat_half2[i, :])
+            feat_half2_2fps.append(feat_aux)
+            
+        feat_half2_2fps.append(feat_half2[-1, :])
+        feat_half2_2fps = np.array(feat_half2_2fps)
+        
         
     print(listGames)
     
