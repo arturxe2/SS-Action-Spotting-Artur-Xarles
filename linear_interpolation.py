3a1e9cd6c4408113684b3e6cdc2ab435ab2cc957
arturxe2 <arturxe@gmail.com>
2022-09-15 09:39:43 +0200

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -6,12 +6,22 @@ Created on Thu Sep 15 09:25:43 2022
 """
 
 from SoccerNet.Downloader import getListGames
+from tqdm import tqdm
+import os
+import numpy as np
+
+
 def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features', 
                         input_name = 'baidu_soccer_embeddings.npy', 
                         output_name = 'baidu_soccer_embeddings_2fps.npy', 
                         split = ['train', 'valid', 'test']):
     
     listGames = getListGames(split)
+    
+    for game in tqdm(listGames):
+        feat_half1 = np.load(os.path.join(path, game, "1_" + input_name))
+        print(feat_half1.shape)
+        
     print(listGames)
     
 LinearInterpolation()
