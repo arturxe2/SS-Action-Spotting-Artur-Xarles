9ec9787ff29d20b26d3bd4855c5a899e2433570a
arturxe2 <arturxe@gmail.com>
2022-09-15 09:32:44 +0200

    Create linear_interpolation.py

---
+++ linear_interpolation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 09:25:43 2022
+
+@author: arturxe
+"""
+
+def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features', 
+                        input_name = 'baidu_soccer_embeddings.npy', 
+                        output_name = 'baidu_soccer_embeddings_2fps.npy', 
+                        split = ['train', 'valid', 'test']):
+    print('Hola')
