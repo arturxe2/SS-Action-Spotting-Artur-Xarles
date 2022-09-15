93db626b9bb15f0eb88bbb102f6f01b3fcf03b5d
arturxe2 <arturxe@gmail.com>
2022-09-15 09:48:28 +0200

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -19,8 +19,13 @@ def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features',
     listGames = getListGames(split)
     
     for game in tqdm(listGames):
-        feat_half1 = np.load(os.path.join(path, game, "1_" + input_name))
-        print(feat_half1.shape)
+        feat_half1 = np.load(os.path.join(path, game, '1_' + input_name))
+        feat_half2 = np.load(os.path.join(path, game, '2_' + input_name))
+        
+        for i in range(len(feat_half1) - 1):
+            feat_aux = (feat_half1[i, :] + feat_half1[i+1, :]) / 2
+            print(feat_aux.shape)
+        print(asdf)
         
     print(listGames)
     
