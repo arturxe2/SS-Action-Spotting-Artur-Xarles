385ac0c490c15f8cae42805bd43cfcecde871e62
arturxe2 <arturxe@gmail.com>
2022-09-15 09:37:08 +0200

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -5,10 +5,13 @@ Created on Thu Sep 15 09:25:43 2022
 @author: arturxe
 """
 
+from SoccerNet.Downloader import getListGames
 def LinearInterpolation(path = '/data-net/datasets/SoccerNetv2/Baidu_features', 
                         input_name = 'baidu_soccer_embeddings.npy', 
                         output_name = 'baidu_soccer_embeddings_2fps.npy', 
                         split = ['train', 'valid', 'test']):
-    print('Hola')
+    
+    listGames = getListGames(split)
+    print(listGames)
     
 LinearInterpolation()
