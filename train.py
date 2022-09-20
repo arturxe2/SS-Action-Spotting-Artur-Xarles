4d8fdd00ad5d45edc4ee88bf57a79ae4caf11e61
arturxe2 <arturxe@gmail.com>
2022-09-20 15:46:29 +0200

    Update train.py

--- train.py
+++ train.py
@@ -157,7 +157,7 @@ def trainerAS(train_loader,
             model_name,
             max_epochs=1000,
             evaluation_frequency=3,
-            freeze = False):
+            freeze = True):
 
     logging.info("start training action spotting")
 
