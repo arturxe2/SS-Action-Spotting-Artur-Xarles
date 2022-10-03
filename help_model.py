87f4f659178ae8dc225e0542171e7e3da10278b6
arturxe2 <arturxe@gmail.com>
2022-10-03 17:51:59 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -15,7 +15,7 @@ model = vit_b_16()
 #model.classifier = torch.nn.Identity()
 print(model)
 
-image = torch.randn([31, 224, 398,3])
+image = torch.randn([31, 3, 224, 398])
 #image = image.view(-1, 3, 224, 398)
 output = model(image)
 print(output.shape)
