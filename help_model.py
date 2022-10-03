25fa517546546b6a3db54ddc74ace3cc72db3d96
arturxe2 <arturxe@gmail.com>
2022-10-03 13:29:20 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -6,10 +6,10 @@ Created on Mon Oct  3 13:16:20 2022
 """
 
 import torch
-from torchvision.models import mobilenet_v3_small
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
 
-model = mobilenet_v3_small()
-print(model)
+model = mobilenet_v3_small(MobileNet_V3_Small_Weights)
+#print(model)
 
 image = torch.randn([10, 3, 224, 398])
 print(model(image).shape)
