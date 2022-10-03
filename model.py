cc0d93a09b34818a3be3bb66265f233ea044f8f0
arturxe2 <arturxe@gmail.com>
2022-10-03 15:34:39 +0200

    Update model.py

--- model.py
+++ model.py
@@ -594,7 +594,7 @@ class ModelAS(nn.Module):
     
 #Model class
 class ModelFrames(nn.Module):
-    def __init__(self, weights=None, num_classes = 17, d=512, chunk_size=10, framerate=2, p_mask = 0.15, framestride = 4, model="SSModel"):
+    def __init__(self, weights=None, num_classes = 17, d=512, chunk_size=5, framerate=2, p_mask = 0.15, framestride = 4, model="SSModel"):
         """
         INPUT: two Tensors of shape (batch_size,chunk_size*framerate,feature_size)
         OUTPUTS: two Tensors of shape (batch_size,d)
@@ -606,7 +606,7 @@ class ModelFrames(nn.Module):
         
         self.num_classes = num_classes
         self.d = d
-        self.chunk_size = chunk_size
+        self.chunk_size = chunk_size * framerate
         self.framerate = framerate
         self.p_mask = p_mask
         self.model = model
@@ -621,7 +621,7 @@ class ModelFrames(nn.Module):
         #Convolutions (reduce dimensionality)
         self.conv1V = nn.Conv1d(576, d, 1, stride=1, bias=False)
         self.conv1A = nn.Conv1d(128, d, 1, stride=1, bias=False)
-        self.norm1V = nn.LayerNorm([self.chunk_size * 25 // self.framestride, d])
+        self.norm1V = nn.LayerNorm([self.chunk_size / framerate * 25 // self.framestride, d])
         self.norm1A = nn.LayerNorm([self.chunk_size * self.framerate, d])
         self.norm2 = nn.LayerNorm([self.chunk_size * self.framerate + 1, d])
         self.norm3 = nn.LayerNorm([2 * self.chunk_size * self.framerate, d])
