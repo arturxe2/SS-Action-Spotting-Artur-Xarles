e995ef6f0ce2fddb37369fa1eea2f562c299f141
arturxe2 <arturxe@gmail.com>
2022-10-03 17:32:56 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -7,12 +7,12 @@ Created on Mon Oct  3 13:16:20 2022
 
 import torch
 
-from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights, mvit_v2_s, MViT_V2_S_Weights
 
-model = mobilenet_v3_small(MobileNet_V3_Small_Weights)
+model = mvit_v2_s(MViT_V2_S_Weights)
 
-model.classifier = torch.nn.Identity()
-#print(model)
+#model.classifier = torch.nn.Identity()
+print(model)
 
 image = torch.randn([10, 31, 3, 224, 398])
 image = image.view(-1, 3, 224, 398)
