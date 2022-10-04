bf2ffda6b65ac1df80a1f7466128617cf13eaa85
arturxe2 <arturxe@gmail.com>
2022-10-04 10:43:20 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -11,7 +11,7 @@ import torchvision.transforms as T
 
 from torchvision.models import swin_t, Swin_T_Weights
 
-model = torchvision.models.swin_t(pretrained = True)
+model = torchvision.models.swin_t(Swin_T_Weights)
 transform = T.Resize((224,224))
 model.head = torch.nn.Identity()
 #model.classifier = torch.nn.Identity()
