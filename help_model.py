e6f81c41c378f60fa708528dd388723f73459fe3
arturxe2 <arturxe@gmail.com>
2022-10-04 10:01:23 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -7,15 +7,20 @@ Created on Mon Oct  3 13:16:20 2022
 
 import torch
 import torchvision
+import torchvision.transforms as T
 
 from torchvision.models import vit_b_16
 
 model = vit_b_16()
+transform = T.Resize((224,224))
 
 #model.classifier = torch.nn.Identity()
 print(model)
 
-image = torch.randn([31, 3, 224, 224])
+image = torch.randn([31, 3, 224, 398])
+images = transform(image)
+
+print(images.shape)
 #image = image.view(-1, 3, 224, 398)
 output = model(image)
 print(output.shape)
