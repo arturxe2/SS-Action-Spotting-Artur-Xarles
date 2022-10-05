95731a443a23ecdbab7678a534400ade84ee197d
arturxe2 <arturxe@gmail.com>
2022-10-05 12:58:37 +0200

    Update help_model.py

--- help_model.py
+++ help_model.py
@@ -12,7 +12,17 @@ from vit_pytorch import ViT
 
 from torchvision.models import swin_s, Swin_S_Weights
 
-model = ViT()
+model = ViT(
+    image_size = 256,
+    patch_size = 32,
+    num_classes = 1000,
+    dim = 1024,
+    depth = 6,
+    heads = 16,
+    mlp_dim = 2048,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
 
 mem_params = sum([param.nelement()*param.element_size() for param in model.parameters()])
 mem_bufs = sum([buf.nelement()*buf.element_size() for buf in model.buffers()])
