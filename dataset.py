d37aa9fb44784dcbe4d17f445a360eabd1d8ffb7
arturxe2 <arturxe@gmail.com>
2022-10-05 13:02:47 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -782,3 +782,12 @@ class OnlineSoccerNetFrames(Dataset):
 
         return(len(self.path_list))
     
+a = OnlineSoccerNetFrames(path_frames = '/data-local/data1-hdd/axesparraguera/SoccerNetFrames', 
+             path_audio = '/data-local/data3-ssd/axesparraguera',  
+             path_labels = "/data-net/datasets/SoccerNetv2/ResNET_TF2",
+             path_store = '/data-local/data1-hdd/axesparraguera/SoccerNetFrames/trial',
+             features_audio = 'audio_embeddings_2fps.npy', 
+             split=["train"], framerate=2, chunk_size=4, framestride = 4, store = True)
+
+a.__getitem__(1)
+a.__getitem__(10000)
