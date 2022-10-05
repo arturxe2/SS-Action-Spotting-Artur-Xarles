4c63b4cd6f4269d464f547163f3afed7587a93e5
arturxe2 <arturxe@gmail.com>
2022-10-05 17:49:37 +0200

    Update axesparraguera_main2.py

--- axesparraguera_main2.py
+++ axesparraguera_main2.py
@@ -67,7 +67,7 @@ def main(args):
     # create model
     model = ModelFrames(weights=args.load_weights, d=args.hidden_d, 
         chunk_size=args.chunk_size, framerate=args.framerate, p_mask=args.p_mask, 
-        model=args.model, backbone = 'vit').cuda()
+        model=args.model, backbone = 'vit', masking = 'frame').cuda()
        
     logging.info(model)
     total_params = sum(p.numel()
